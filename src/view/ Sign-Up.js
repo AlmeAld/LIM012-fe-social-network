75c73851db539c2ab3cfeb82f331c32a763c7648
almeald <almenaldana@gmail.com>
2020-06-10 06:31:50 -0500

    style
    log in layout

---
+++ src/view/ Sign-Up.js
@@ -0,0 +1,19 @@
+export default ()=>{
+const viewSingUp = `
+  <img src="/src/img/mochilero.gif" alt="gif de una mochila">
+    <h1>TripVenture</h1>
+    <form action="/signup">
+      <input type="email" placeholder="e-mail" required>
+      <input type="password" placeholder="contraseña" minlength="8" required>
+      <button>Iniciar Sesión</button>
+    </form>
+    <p>O bien ingresa con...<p/>
+    <i class="fab fa-facebook"></i>
+    <i class="fab fa-google"></i>
+`
+
+
+}
+
+{/* <p>¡Hola Viajero!<span>ingrese su información a continuación para registrarte</span></p> 
+<input type="text" placeholder="Nombre y Apellido" required>*/}
