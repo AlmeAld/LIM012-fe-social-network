b55c2257dcc967c3a074bc19cfa5be286d8f19e0
almeald <almenaldana@gmail.com>
2020-06-17 01:28:08 -0500

    agregando los componentes home y not found

--- src/view/index.js
+++ src/view/index.js
@@ -1,9 +1,13 @@
 import signIn from './sign-in.js';
 import signUp from './sign-up.js';
+import home from './home.js';
+import notfound from './404.js'
 
 const obComponents = {
 signin: signIn(),
-signup: signUp()
+signup: signUp(),
+home: home(),
+notfound: notfound()
 }
 
 
