df17b50cf0e2c81dfde3ba19f85b4fc0094af73c
almeald <almenaldana@gmail.com>
2020-07-03 00:35:33 -0500

    agregando el observador que verifica si el usuario esta logueado para que lo lleve a la vista home

--- src/index.js
+++ src/index.js
@@ -3,13 +3,46 @@
 // example();
 
 
-import { changeView } from './router/index.js'
+import { changeView } from './router/index.js';
+// import { firebaseInit } from './firebase-config.js';
+
+
 
 const init = () => {
-  changeView(window.location.hash)
+  changeView(window.location.hash);
   window.addEventListener('hashchange', () => {
     changeView(window.location.hash);
-  })
-}
+  });
+  // firebaseInit();
+  firebase.auth().onAuthStateChanged((user) => {
+    if (user) {
+      // /si user existe signfica que te logueaste
+      // localStorage.setItem('uid', user.uid)
+      window.location.has = '#/home';
+    } else {
+      window.location.hash = '#/';
+    }
+  });
+};
 
-window.addEventListener('load', init)
+window.addEventListener('load', init);
+// window.addEventListener('hashchange', init);
+
+// const init = () => {
+
+//   changeView(window.location.hash);
+//   window.addEventListener('hashchange', () => {
+//     changeView(window.location.hash);
+//   })
+//   firebaseInit();
+//   firebase.auth().onAuthStateChanged((user) => {
+//     if (user) {
+//       window.location.hash = '#/signIn'
+//     } else {
+//       window.location.hash = '#/home'
+
+//     }
+//   })
+// }
+
+// window.addEventListener('load', init)
