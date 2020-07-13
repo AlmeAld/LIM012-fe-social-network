76752c3df06cf2439c4207de4217b033ed54af47
almeald <almenaldana@gmail.com>
2020-07-13 13:45:18 -0500

    refactorizando codigo para la funcion createUser

--- src/model/auth.js
+++ src/model/auth.js
@@ -6,9 +6,9 @@ export const createUser = (email, password, Name)=>{
   firebase.auth().createUserWithEmailAndPassword(email, password)
   .then(result=>{
     console.log('cuenta creada')
-    // result.user.updateProfile({
-    //   displayName : Name
-    // })
+    result.user.updateProfile({
+      displayName : Name
+    })
     const configuracion = {
       url: 'http://localhost:5000/#/signIn'
     }
@@ -40,6 +40,7 @@ export const createUser = (email, password, Name)=>{
 export const login = (emailLogin, passwordLogin)=>{
   firebase.auth().signInWithEmailAndPassword(emailLogin,passwordLogin)
     .then((userLogin)=>{
+      console.log(userLogin);
       window.location.hash = '#/home'
       console.log('estoy logeado weeey')
       console.log(userLogin);
