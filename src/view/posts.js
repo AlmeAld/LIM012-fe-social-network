2310f3082ca2c6047cdec471b9f467e268f847a7
almeald <almenaldana@gmail.com>
2020-07-20 22:56:50 -0500

    template de los posts con modal

--- src/view/posts.js
+++ src/view/posts.js
@@ -1,25 +1,85 @@
-export default()=>{
+import{postPrivacy} from '../model/firestore.js'
+export default(data)=>{
   const postContent = document.createElement('div');
   postContent.classList.add('content-posts');
   postContent.innerHTML= `
-   <div class = 'content-posts'>
-        <div class= 'info-profile'>
-        <img src="../img/profile1.png" alt="avatar" width = 120px class = 'photo-user'>
-        <h3 class = 'name-user'>Fulana Suarez</h3>
-        <p>5 de julio de 2020</p>
-        <i class="fas fa-ellipsis-h"></i>
-        <div class = 'option-publication' >
-        <i class="fas fa-pen">Editar</i>
-        <i class="fas fa-globe-americas"> Público</i>
-        <i class="fas fa-lock">Privado</i>
-        <i class="far fa-trash-alt">Eliminar</i>
-        </div>
-        <div class='content-message'>
-          <p>Hola soy fulanas</p>
-        </div>
-        <i class="far fa-heart"></i>
-        <i class="far fa-comment-alt"></i>
-      </div>`
+    <div class= 'data-user'>
+      <img src=${data.photoURL} alt="avatar" s class = 'photo-user'>
+      <h3 class = 'name-user'>${data.userName}</h3>
+      <p class='hour'>${data.postHora} </p>
+      <div class = 'option-publication'></div>
+    </div>
+  
+    <div class='content-message'>
+      <p class='message'>${data.posts}</p>
+    </div>
+    <div class= 'like-coment'>
+      <button class ='like'>
+        <i class="far fa-heart fa-lg"></i>
+      </button>
+              0 
+      <button class=coment>
+        <i class="far fa-comment-alt fa-lg"></i>
+      </button>
+    </div>`
 
+  //modal
+  const modalContainer = postContent.querySelector('.option-publication')
+  modalContainer.innerHTML = `
+            <div class='o-publication'>
+              <i class="fas fa-ellipsis-h"></i>
+              <div class='content-option-post'>
+                <ul>
+                  <li class='setting-post edit-post'>
+                    <i class="fas fa-pen"></i>Edit
+                  </li>
+                  <li  class='setting-post public-post'>
+                    <i class="fas fa-globe-americas"></i>Public
+                  </li>
+                  <li  class='setting-post private-post'>
+                    <i class="fas fa-lock"></i>Private
+                  </li>
+                  <li  class='setting-post remove-post'>
+                    <i class="fas fa-trash-alt"></i>Delete
+                  </li>
+                </ul>
+              </div>
+            </div>
+          `
+
+  const btnModal = postContent.querySelector('.o-publication')
+  // evento para editar post
+  const optionEditPost = btnModal.querySelector('.edit-post')
+  optionEditPost.addEventListener('click', () => {
+    console.log('click en editar post');
+  })
+
+  //evento para poner el post en publico
+  const optionPublicPost = btnModal.querySelector('.public-post')
+  optionPublicPost.addEventListener('click', () => {
+    postPrivacy(data.id, 'public')
+  })
+  //evento para poner el post en privado
+  const optionPrivatePost = btnModal.querySelector('.private-post')
+  optionPrivatePost.addEventListener('click', () => {
+    console.log('click en post privado');
+    postPrivacy(data.id, 'private')
+
+  })
+  //evento para poner eliminar un post
+  const optionRemovePost = btnModal.querySelector('.remove-post')
+  optionRemovePost.addEventListener('click', () => {
+    console.log('click en eliminar post');
+  })
+
+  //funcionalidad a opciones del Post publicado
+  window.addEventListener('click', (e) => {
+    if (btnModal.contains(e.target)) {
+      btnModal.classList.toggle('open');
+    } else {
+      btnModal.classList.remove('open');  
+    }
+    
+  });
   return postContent
 }
