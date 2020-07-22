a7c08ca2845c3ae56c41b15d443bcb1c51a5952e
almeald <almenaldana@gmail.com>
2020-07-22 01:48:50 -0500

    update contentpost

--- src/model/firestore.js
+++ src/model/firestore.js
@@ -1,11 +1,31 @@
 import{db} from '../firebase-config.js'
 
+
 //update post
+// userName: displayName,
+// photoURL: photo,
+//imgPost// img: imgPost
 export const postPrivacy =(id, status)=>{
   db.collection('posts').doc(id).update({
     postStatus: status,
   })
 }
+export const editPost = (id, contentPost)=>{
+  db.collection('posts').doc(id).update({
+    posts: contentPost,
+  })
+}
+
+//create posts 
+// export const createCollection = (uid, hora, displayName,contentPost,  status)=>{
+//   db.collection('posts').add({
+//     id:uid,
+//     postHora: hora,
+//     userName: displayName,
+//     contentpost: contentPost,
+//     postStatus: status,
+//   })
+// }
 
 //get post
 export const getPost = (callback) => {
@@ -21,11 +41,12 @@ export const getPost = (callback) => {
           userName:doc.data().userName,
           postHora: doc.data().postHora,
           posts: doc.data().posts,
+          img: doc.data().img,
         }
         data.push(obj)
+        console.log(doc.id);
       })
       callback(data);
-      // console.log(data);
     })
   }
 
